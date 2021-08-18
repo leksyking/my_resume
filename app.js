fb5a7f6044651ffc4a086def7cce16f3d7c5c582
Felix <gbemilekeogundipe@gmail.com>
2021-08-18 02:53:13 -0700

    Second Commit

--- app.js
+++ app.js
@@ -22,4 +22,4 @@ app.post("/contact.html", (req, res) => {
     res.send(`${name}! Thank you for contacting me!`);
 })
 
-app.listen(process.env.PORT || 3000, () => console.log('Starting app.'));
+app.listen(process.env.PORT, () => console.log('Starting app.'));
